94cd4683f0a2d698e3e793fe1f5f28ca4029091e
Michael Samuel <mik@miknet.net>
2013-09-27 15:28:11 +1000

    Unbiased random, fallback to abort() instead of drand48()

--- randnum.c
+++ randnum.c
@@ -7,6 +7,7 @@
  * License.
  */
 
+#include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <sys/types.h>
@@ -14,43 +15,32 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <errno.h>
+#include <string.h>
 
 #include "pwgen.h"
 
-#ifdef HAVE_DRAND48
-extern double drand48(void);
+#ifdef NDEBUG
+#undef NDEBUG
 #endif
+#include <assert.h>
 
 static int get_random_fd(void);
+
+/* Platform-specific random devices can be ifdef'd here */
+#define RANDOM_DEVICE "/dev/urandom"
 
 /* Borrowed/adapted from e2fsprogs's UUID generation code */
 static int get_random_fd()
 {
-	struct timeval	tv;
 	static int	fd = -2;
-	int		i;
 
 	if (fd == -2) {
-		gettimeofday(&tv, 0);
-		fd = open("/dev/urandom", O_RDONLY);
-		if (fd == -1)
-			fd = open("/dev/random", O_RDONLY | O_NONBLOCK);
-#ifdef HAVE_DRAND48
-		srand48((tv.tv_sec<<9) ^ (getpgrp()<<15) ^
-			(getpid()) ^ (tv.tv_usec>>11));
-#else
-		srandom((getpid() << 16) ^ (getpgrp() << 8) ^ getuid() 
-		      ^ tv.tv_sec ^ tv.tv_usec);
-#endif
+		fd = open(RANDOM_DEVICE, O_RDONLY);
+		if (fd == -1) {
+			fprintf(stderr, "Unable to open %s: %s\n", RANDOM_DEVICE, strerror(errno));
+			abort(); /* Something is seriously wrong with this system!! */
+		}
 	}
-	/* Crank the random number generator a few times */
-	gettimeofday(&tv, 0);
-	for (i = (tv.tv_sec ^ tv.tv_usec) & 0x1F; i > 0; i--)
-#ifdef HAVE_DRAND48
-		drand48();
-#else
-		random();
-#endif
 	return fd;
 }
 
@@ -62,34 +52,34 @@ int pw_random_number(max_num)
 	int max_num;
 {
 	int i, fd = get_random_fd();
-	int lose_counter = 0, nbytes=4;
+	int nbytes=4;
 	unsigned int rand_num;
 	char *cp = (char *) &rand_num;
 
-	if (fd >= 0) {
-		while (nbytes > 0) {
-			i = read(fd, cp, nbytes);
-			if ((i < 0) &&
-			    ((errno == EINTR) || (errno == EAGAIN)))
-				continue;
-			if (i <= 0) {
-				if (lose_counter++ == 8)
-					break;
-				continue;
-			}
-			nbytes -= i;
-			cp += i;
-			lose_counter = 0;
-		}
+	int bits = 0, b = max_num;
+	while(b > 0) {
+		bits += 1;
+		b >>= 1;
 	}
-	if (nbytes == 0)
-		return (rand_num % max_num);
 
-	/* OK, we weren't able to use /dev/random, fall back to rand/rand48 */
+	assert(fd >= 0);
 
-#ifdef HAVE_DRAND48
-	return ((int) ((drand48() * max_num)));
-#else
-	return ((int) (random() / ((float) RAND_MAX) * max_num));
-#endif
+	while (nbytes > 0) {
+		i = read(fd, cp, nbytes);
+		if ((i < 0) && ((errno == EINTR) || (errno == EAGAIN)))
+			continue;
+		if (i <= 0) {
+			fprintf(stderr, "Random device is broken: %s\n", strerror(errno));
+			abort();
+		}
+		nbytes -= i;
+		cp += i;
+	}
+
+	assert(nbytes == 0);
+
+	rand_num &= ((1<<bits)-1);
+	if(rand_num >= max_num)
+		return pw_random_number(max_num);
+	return rand_num;
 }
