b0556c61b395cf89af9bee739a24b6f0b54c661e
Jiri Kukacka <jiri.kukacka@oracle.com>
2013-12-10 14:22:55 +0100

    Use system sha1 library instead of local implementation.

--- sha1num.c
+++ sha1num.c
@@ -11,7 +11,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "pwgen.h"
+#ifdef SHA_INTERNAL
 #include "sha1.h"
+#endif
+#ifdef SHA_SOLARIS
+#include <sha1.h>
+#define sha1_context SHA1_CTX
+#define sha1_starts SHA1Init
+#define sha1_update SHA1Update
+#define sha1_finish(a,b) SHA1Final(b,a)
+#endif
 
 sha1_context sha1_ctx;
 char *sha1_seed;
